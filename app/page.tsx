06f693343bfae5a649d61efa9cb2baeaf29c12e5
utibe edet <utibeabasi.okon@myrecharge.ng>
2024-07-03 13:05:55 -0700

    latest update u

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,14 @@ export default function App() {
      
       {/* Roulette component */}
       <Roulette />
+      <section className="flex bg-[#021E35] justify-between items-center w-full text-white h-[10vh]">
+        <div className="bg-[#021E35] border border-1 w-[25%] h-full flex justify-center items-center">Presale </div>
+        <div className="w-[25%] bg-[#F8B214] border border-1 h-full flex justify-center items-center">Referral</div>
+        <div className="w-[25%] bg-[#02B68F]  border border-1 h-full flex justify-center items-center">Task</div>
+        <div className="w-[25%] bg-white text-blue-700 h-full flex justify-center items-center">
+          <a href="/mypoint">Withdraw</a>
+           </div>
+      </section>
     </div>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,14 +6,7 @@ const Footer = () => {
 
   return (
     
-       <section className="flex bg-[#021E35] justify-between items-center w-full text-white h-[10vh]">
-        <div className="bg-[#021E35] border border-1 w-[25%] h-full flex justify-center items-center">Presale </div>
-        <div className="w-[25%] bg-[#F8B214] border border-1 h-full flex justify-center items-center">Referals</div>
-        <div className="w-[25%] bg-[#02B68F]  border border-1 h-full flex justify-center items-center">Task</div>
-        <div className="w-[25%] bg-white text-blue-700 h-full flex justify-center items-center">
-          <a href="/mypoint">Withdraw</a>
-           </div>
-      </section>
+      <div></div>
     
   );
 };
