b8dffddd17594033a88e0280b5b5ef4eb62bc7ff
utibe edet <utibeabasi.okon@myrecharge.ng>
2024-07-13 15:53:34 -0700

    latest update x6

--- app/mytask/page.tsx
+++ app/mytask/page.tsx
@@ -4,10 +4,10 @@ const page = () => {
   return (
     <div className="App px-5 md:px-10 pt-11">
       <div className="bg-gray-50 py-2 shadow-sm border-b">
-        <span className="">
-         <span className="font-bold">1:</span>  "Use your referral link on the
-          dashboard and refer users to win a massive 500,000 points!"
-        </span>
+       
+         <span className="font-bold">1:</span>  Use your referral link on the
+          dashboard and refer users to win a massive 500,000 points!
+       
       </div>
     </div>
   );
