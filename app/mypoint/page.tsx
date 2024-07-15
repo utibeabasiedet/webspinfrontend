f0983c638860bbd4cbd528899e518ae793c82dff
utibe edet <utibeabasi.okon@myrecharge.ng>
2024-07-15 15:14:01 -0700

    latest update x6

--- app/mypoint/page.tsx
+++ app/mypoint/page.tsx
@@ -169,7 +169,7 @@ function App() {
             <DialogHeader>
               <DialogTitle>Edit profile</DialogTitle>
               <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
+                Make changes to your profile here. Click save when you are done.
               </DialogDescription>
             </DialogHeader>
             <div className="grid gap-4 py-4">
